f02bf4fba51657d4a2a46418315e1c01ae2c24dc
StrikerOne65 <golamrabbani20014@gmail.com>
2021-12-18 02:23:15 +0530

    Adding firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { auth, db } from './lib/firebase'
+import { RecaptchaVerifier, signInWithPhoneNumber } from 'firebase/auth'
 
 function App() {
+  const [usn, setUsn] = useState('')
+  const [final, setFinal] = useState()
+  const [show, setShow] = useState(false)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    let verify = new RecaptchaVerifier('captcha', { size: 'invisible' }, auth)
+
+    // signInWithPhoneNumber(auth, '+911111111111', verify)
+    //   .then((confirm) => {
+    //     console.log('sent', confirm)
+    //   })
+    //   .catch((err) => {
+    //     console.log(err)
+    //   })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <nav>
+        <div className='logo'>
+          SaiT<span>FeedBack</span>
+        </div>
+      </nav>
+      <div className='app'>
+        <h1>Welcome to SaITFeedBack</h1>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor='usn'>Enter Your USN</label>
+          <div id='captcha' className='captcha'></div>
+          <input
+            name='usn'
+            id='usn'
+            placeholder='Enter Your USN'
+            value={usn}
+            onChange={(e) => setUsn(e.target.value)}
+          />
+          <button type='submit'>Next</button>
+        </form>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
